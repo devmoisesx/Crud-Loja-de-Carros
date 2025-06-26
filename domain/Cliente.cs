49e0aefee7ff8c0f6ac9a46b0fed12eb7b8ffe90
Moises_Xavier <secondmox91@gmail.com>
2025-06-26 19:02:05 -0300

    Create Client Class

---
+++ domain/Cliente.cs
@@ -0,0 +1,13 @@
+public class Cliente
+{
+    private string nome;
+    private string documento;
+    // private List<ClienteContato> contato;
+    private string logradouro;
+    private string casaNumero;
+    private string bairro;
+    private string complemento;
+    private string cidade;
+    private string estado;
+    private int cep;
+}
