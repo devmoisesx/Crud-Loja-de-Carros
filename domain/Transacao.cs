0a8d52e0c6a50010def16ec02a9287c9b70d8bd7
Moises_Xavier <secondmox91@gmail.com>
2025-06-27 05:13:59 -0300

    Add interface methods

--- domain/Transacao.cs
+++ domain/Transacao.cs
@@ -1,11 +1,13 @@
-public class Transacao
+public class Transacao : IServiceTransacoes
 {
-    private int id;
-    private TipoTransacao tipo;
-    private int carroID;
-    private int clienteID;
-    private float valor;
-    private DateTime data;
+    public int Id { get; set; }
+    public TipoTransacao Tipo { get; set; }
+    public int CarroID { get; set; }
+    public int ClienteID { get; set; }
+    public float Valor { get; set; }
+    public DateTime Data { get; set; }
+
+    public static List<Transacao> TodasTransacoes = new List<Transacao>();
 
     public enum TipoTransacao
     {
@@ -15,11 +17,34 @@ public class Transacao
 
     public Transacao(int id, TipoTransacao tipo, int carroID, int clienteID, float valor, DateTime data)
     {
-        this.id = id;
-        this.tipo = tipo;
-        this.carroID = carroID;
-        this.clienteID = clienteID;
-        this.valor = valor;
-        this.data = data;
+        Id = id;
+        Tipo = tipo;
+        CarroID = carroID;
+        ClienteID = clienteID;
+        Valor = valor;
+        Data = data;
+    }
+
+    public List<Transacao> ListarTransacoes(TipoTransacao tipo, int mes)
+    {
+        return TodasTransacoes;
+    }
+
+    public Transacao ListarTransacao(int id)
+    {
+        foreach (var transacao in TodasTransacoes)
+        {
+            if (transacao.Id == id)
+            {
+                return transacao;
+            }
+        }
+        
+        return null;
+    }
+
+    public int InsereTransacao(TipoTransacao tipo, int clienteID, int carroID, DateTime data, float valor)
+    {
+        return 1;
     }
 }
