4636bb65f6712fd3ec7b6a46a9eaeef9c4bc556d
Moises_Xavier <secondmox91@gmail.com>
2025-06-27 05:13:08 -0300

    Change parameter type

--- services/interfaces/IServiceTransacoes.cs
+++ services/interfaces/IServiceTransacoes.cs
@@ -1,6 +1,6 @@
-// interface IServiceTransacoes
-// {
-//     List<Transacao> ListarTransacoes(TipoTransacao tipo, int mes);
-//     Transacao ListaTransacao(int id);
-//     int InsereTransacao(TipoTransacao tipo, int clienteID, int carroID, DateTime data, float valor);
-// }
+interface IServiceTransacoes
+{
+    List<Transacao> ListarTransacoes(Transacao.TipoTransacao tipo, int mes);
+    Transacao ListarTransacao(int id);
+    int InsereTransacao(Transacao.TipoTransacao tipo, int clienteID, int carroID, DateTime data, float valor);
+}
