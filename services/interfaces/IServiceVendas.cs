1f6994861c98e57e0aff5968c7f931681aa16be1
Moises_Xavier <secondmox91@gmail.com>
2025-06-27 04:57:46 -0300

    Change name of Service

--- services/interfaces/IServiceVendas.cs
+++
@@ -1,6 +0,0 @@
-interface IServiceVendas
-{
-    List<Transacao> ListarTransacoes(TipoTransacao tipo, int mes);
-    Transacao ListaTransacao(int id);
-    int InsereTransacao(TipoTransacao tipo, int clienteID, int carroID, DateTime data, float valor);
-}
