690bb656119e75d421fdbc5e8b7224b796be6f59
Moises_Xavier <secondmox91@gmail.com>
2025-06-27 03:09:05 -0300

    Correct the names of variables

--- domain/Carro.cs
+++ domain/Carro.cs
@@ -1,27 +1,64 @@
 public class Carro : IServiceCarro
 {
-    private int id;
-    private int anoFabricacao;
-    private string modelo;
-    private int anoModelo;
-    private int km;
-    // private TipoTransmissao transmissao;
-    private float valor;
-    private string cor;
-    private string chassis;
-    // private HashSet<CarroOpcionais> opcionais;
+    public MarcaCarro Marca { get; set; }
+    public int Id { get; set; }
+    public int AnoFabricacao { get; set; }
+    public string Modelo { get; set; }
+    public int AnoModelo { get; set; }
+    public int Km { get; set; }
+    public TipoTransmissao Transmissao { get; set; }
+    public float Valor { get; set; }
+    public string Cor { get; set; }
+    public string Chassis { get; set; }
+    // public HashSet<CarroOpcionais> opcionais { get; set; }
 
-    public Carro(int id, string modelo, int anoFabricacao, int anoModelo, int km, float valor, string cor, string chassis)
+    public enum MarcaCarro
     {
-        this.id = id;
-        this.anoFabricacao = anoFabricacao;
-        this.modelo = modelo;
-        this.anoModelo = anoModelo;
-        this.km = km;
-        // this.transmissao = transmissao;
-        this.valor = valor;
-        this.cor = cor;
-        this.chassis = chassis;
+        VW = 0,
+        GM = 1,
+        Ford = 2,
+        Toyota = 3,
+        Honda = 4
+    }
+    
+    public enum TipoTransmissao
+    {
+        Manual = 0,
+        Automática = 1,
+        Automatizado = 2,
+        CVT = 3,
+        DSG = 4
+    }
+
+    // private const CarroOpcionais {
+	// 	Blindado,
+	// 	Direção Hidraulíca,
+	// 	Teto Solar,
+	// 	Vidros Elétricos,
+	// 	Bancos em Couro,
+	// 	Cam Traseira,
+	// 	Ar Condicionado,
+	// 	Central Multimídia,
+	// 	Radio
+	// }
+
+    public static List<Carro> TodosCarros = new List<Carro>();
+
+    public Carro() { }
+
+    public Carro(MarcaCarro marca, int id, string modelo, int anoFabricacao, int anoModelo, int km, TipoTransmissao transmissao, float valor, string cor, string chassis)
+    {
+        this.Marca = marca;
+        this.Id = id;
+        this.AnoFabricacao = anoFabricacao;
+        this.Modelo = modelo;
+        this.AnoModelo = anoModelo;
+        this.Km = km;
+        this.Transmissao = transmissao;
+        this.Valor = valor;
+        this.Cor = cor;
+        this.Chassis = chassis;
+        TodosCarros.Add(this);
     }
 
     public int CadastrarCarro(Carro carro)
@@ -36,12 +73,11 @@ public class Carro : IServiceCarro
 
     public List<Carro> ListarCarros()
     {
-        List<Carro> lista = new List<Carro>();
-        return lista;
+        return TodosCarros;
     }
 
     public override string ToString()
     {
-        return $"Carro: \nID: {id} \nAno de Fabricacao: {anoFabricacao} \nModelo: {modelo} \nKM: {km} \nValor: {valor} \nCor: {cor} \nChassis: {chassis}";
+        return $"Carro: \nMarca: {Marca} \nAno de Fabricacao: {AnoFabricacao} \nModelo: {Modelo} \nAno do Modelo: {AnoModelo} \nKM: {Km} \nTipo de Transmissão: {Transmissao} \nValor: {Valor} \nCor: {Cor} \nChassis: {Chassis}";
     }
 }
