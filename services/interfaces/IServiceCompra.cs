32f480be76cf460f3d829aafca2037131d8d96ca
Moises_Xavier <secondmox91@gmail.com>
2025-07-11 12:45:01 -0300

    Remove classes inuteis

--- services/interfaces/IServiceCompra.cs
+++
@@ -1,6 +0,0 @@
-interface IServiceCompra
-{
-    List<Compra> ListarCompras(int mes);
-    Compra ListarCompra(int id);
-    int InsereCompra(int clienteID, int carroID, DateTime data, float valor);
-}
