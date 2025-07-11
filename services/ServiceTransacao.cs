9e85ec486238dccaff4ebac5d8a92bc8840a1b05
Moises_Xavier <secondmox91@gmail.com>
2025-07-11 03:57:43 -0300

    Cria o Servico

---
+++ services/ServiceTransacao.cs
@@ -0,0 +1,18 @@
+public class ServiceTransacao : IServiceTransacao
+{
+    private readonly StorageTransacaoSqlite _storage = new StorageTransacaoSqlite();
+
+    public List<Transacao> ListarTransacoes(Transacao.TipoTransacao tipo, string mes)
+    {
+        return _storage.ListarTransacoes(tipo, mes);
+    }
+
+    public Transacao ListarTransacao(int id)
+    {
+        return _storage.ListarTransacao(id);
+    }
+
+    public void InserirTransacao(Transacao transacao) {
+        _storage.InserirTransacao(transacao);
+    }
+}
