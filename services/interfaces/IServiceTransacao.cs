7ce23d1caafc02de2204b14d890b4a7c87064ac0
Moises_Xavier <secondmox91@gmail.com>
2025-07-11 03:55:39 -0300

    Altera o nome e adiciona metodos

---
+++ services/interfaces/IServiceTransacao.cs
@@ -0,0 +1,6 @@
+interface IServiceTransacao
+{
+    List<Transacao> ListarTransacoes(Transacao.TipoTransacao tipo, string mes);
+    Transacao ListarTransacao(int id);
+    void InserirTransacao(Transacao transacao);
+}
--- services/interfaces/IServiceTransacoes.cs
+++
@@ -1,6 +0,0 @@
-interface IServiceTransacoes
-{
-    List<Transacao> ListarTransacoes(Transacao.TipoTransacao tipo, int mes);
-    Transacao ListarTransacao(int id);
-    int InsereTransacao(Transacao.TipoTransacao tipo, int clienteID, int carroID, DateTime data, float valor);
-}
