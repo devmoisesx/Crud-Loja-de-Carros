93cd9bbd1b8cb22d4157d7b9aa7b1c658795c138
Moises_Xavier <secondmox91@gmail.com>
2025-07-11 14:55:14 -0300

    Adiciona um constructor

--- services/ServiceCarro.cs
+++ services/ServiceCarro.cs
@@ -1,6 +1,11 @@
 public class ServiceCarro : IServiceCarro
 {
-    private readonly StorageCarroSqlite _storage = new StorageCarroSqlite();
+    private readonly StorageCarroSqlite _storage;
+
+    public ServiceCarro(StorageCarroSqlite storage)
+    {
+        _storage = storage;
+    }
 
     public void CadastrarCarro(Carro carro)
     {
